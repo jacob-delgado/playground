3e6148090329f5c875d29163922a2707534bcbcf
Jacob Delgado <jacob.delgado@gmail.com>
2022-07-04 16:14:54 -0600

    Setup opentelemetry for tracing

--- go.mod
+++ go.mod
@@ -5,11 +5,16 @@ go 1.18
 require (
 	github.com/prometheus/client_golang v1.12.2
 	github.com/spf13/cobra v1.5.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.32.0
+	go.opentelemetry.io/otel v1.7.0
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/felixge/httpsnoop v1.0.2 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
@@ -17,6 +22,8 @@ require (
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	go.opentelemetry.io/otel/metric v0.30.0 // indirect
+	go.opentelemetry.io/otel/trace v1.7.0 // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
 )
--- pkg/http/server.go
+++ pkg/http/server.go
@@ -3,14 +3,22 @@ package http
 import (
 	"fmt"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
 	"net/http"
 )
 
+var tracer = otel.Tracer("github.com/jacob-delgado/playground")
+
 func hello(w http.ResponseWriter, req *http.Request) {
+	req.Context()
 	fmt.Fprintf(w, "hello\n")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
+	_, span := tracer.Start(req.Context(), "sleep")
+	defer span.End()
+
 	for name, headers := range req.Header {
 		for _, h := range headers {
 			fmt.Fprintf(w, "%v: %v\n", name, h)
@@ -19,8 +27,14 @@ func headers(w http.ResponseWriter, req *http.Request) {
 }
 
 func Serve(errCh chan error) {
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
+	helloHandler := http.HandlerFunc(hello)
+	wrappedHelloHandler := otelhttp.NewHandler(helloHandler, "hello")
+	http.Handle("/hello", wrappedHelloHandler)
+
+	headerHandler := http.HandlerFunc(headers)
+	wrappedHeaderHandler := otelhttp.NewHandler(headerHandler, "headers")
+	http.Handle("/headers", wrappedHeaderHandler)
+
 	http.Handle("/metrics", promhttp.Handler())
 
 	errCh <- http.ListenAndServe(":8090", nil)
