d8c139867389b2246c2c1caf7e819b17d8c5f3e0
Jacob Delgado <jacob.delgado@gmail.com>
2022-07-04 18:32:47 -0600

    Unignore playground directories

---
+++ cmd/playground/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+
+	"github.com/uptrace/opentelemetry-go-extra/otelzap"
+	"go.uber.org/zap"
+
+	"github.com/spf13/cobra"
+
+	"github.com/jacob-delgado/playground/pkg/http"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "playground",
+	Short: "A sample application to serve files over http/grpc/xds",
+	Long: `A sample application that can be used
+for experimentation with various tools.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		zapLogger, err := zap.NewProduction()
+		if err != nil {
+			panic("could not initialize zap logger")
+		}
+		logger := otelzap.New(zapLogger)
+		defer func() {
+			_ = logger.Sync()
+		}()
+
+		logger.Info("starting playground server")
+
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt)
+
+		errCh := make(chan error)
+		server := http.NewServer(logger)
+		go server.Serve(errCh)
+
+		select {
+		case <-sigCh:
+			logger.Info("exiting")
+			return
+		case err := <-errCh:
+			if err != nil {
+				logger.Panic("exiting due to error", zap.Error(err))
+			}
+		}
+	},
+}
+
+func main() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
