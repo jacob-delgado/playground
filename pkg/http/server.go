9876f940eff5d264f7d16e88c343a58d06ded900
Jacob Delgado <jacob.delgado@gmail.com>
2022-07-04 19:18:21 -0600

    Add prometheus metrics

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -4,7 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/uptrace/opentelemetry-go-extra/otelzap"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
@@ -12,6 +15,26 @@ import (
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
+
+var (
+	headersProcessed = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "headers_called",
+			Help: "The total number of times headers routed was called",
+		},
+		[]string{"foo", "foobar"},
+	)
+
+	helloProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "hello_called",
+		Help: "The total number of times hello route was called",
+	})
+)
+
+func init() {
+	prometheus.MustRegister(headersProcessed)
+	prometheus.MustRegister(helloProcessed)
+}
 
 type Server struct {
 	tracer trace.Tracer
@@ -26,21 +49,26 @@ func NewServer(logger *otelzap.Logger) *Server {
 }
 
 func (s *Server) hello(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	_, span := s.tracer.Start(ctx, "headers")
+	defer span.End()
+
 	fmt.Fprintf(w, "hello\n")
 
 	// add an event for the span with zap using otel
-	ctx := req.Context()
 	s.logger.Ctx(ctx).Error("hello",
 		zap.Error(errors.New("hello")),
 		zap.String("foo", "bar"))
+
+	helloProcessed.Inc()
 }
 
 func (s *Server) headers(w http.ResponseWriter, req *http.Request) {
-	_, span := s.tracer.Start(req.Context(), "sleep")
+	ctx := req.Context()
+	_, span := s.tracer.Start(ctx, "headers")
 	defer span.End()
 
 	// add an event for the span with zap using otel
-	ctx := req.Context()
 	s.logger.Ctx(ctx).Error("headers",
 		zap.Error(errors.New("headers")),
 		zap.String("bar", "baz"))
@@ -48,6 +76,15 @@ func (s *Server) headers(w http.ResponseWriter, req *http.Request) {
 	for name, headers := range req.Header {
 		for _, h := range headers {
 			fmt.Fprintf(w, "%v: %v\n", name, h)
+
+			// cardinality issues will be present, but this isn't production anyway
+			if strings.ToUpper(name) == "FOO" {
+				if strings.ToUpper(h) == "BAR" {
+					headersProcessed.With(prometheus.Labels{"foobar": h}).Inc()
+				} else {
+					headersProcessed.With(prometheus.Labels{"foo": h}).Inc()
+				}
+			}
 		}
 	}
 }
